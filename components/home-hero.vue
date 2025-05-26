826cd6af5975f7c29e803e1dc9bd5e4d14f1811c
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2025-05-26 02:02:48 -0400

    sammy terry lives

--- components/home-hero.vue
+++ components/home-hero.vue
@@ -2,7 +2,7 @@
   <section class="h-screen w-full bg-cover bg-center flex items-center justify-center text-center" style="background-image: url('/sammy-hero.png')">
     <div class=" p-8 rounded-xl">
       <h1 class="text-5xl md:text-7xl font-bold font-creepster mb-4 text-red-800 text-shadow-md ">Welcome to Sammy Terry</h1>
-      <p class="text-lg md:text-2xl text-secondary text-shadow-xs">Chilling your nights since 1962...</p>
+      <p class="text-lg md:text-2xl text-secondary text-shadow-xs">Sammy Terry Lives Forever...</p>
       <UButton to="/home" color="secondary" variant="outline" size="xl" class="mt-6">Join the Nightmare</UButton>
     </div>
   </section>
